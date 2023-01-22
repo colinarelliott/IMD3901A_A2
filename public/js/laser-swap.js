a53aded7fabe6ecbaeddcc200d6bc074cbea2de6
colinarelliott <colinarelliott@gmail.com>
2023-01-22 16:15:27 -0500

    updatedlaserswap cant find prop for changing hands

--- public/js/laser-swap.js
+++ public/js/laser-swap.js
@@ -1,16 +1,27 @@
-
-//attempting to make a script that allows you to switch the laser between hands by pressing the grip button
+//a script that allows you to switch the laser between hands by pressing the grip button
 
 AFRAME.registerComponent('laser-swap',{
     init: function () {
         const CONTEXT_AF = this;
         CONTEXT_AF.leftController = document.querySelector("#otc-left");
         CONTEXT_AF.rightController = document.querySelector("#otc-right");
-        CONTEXT_AF.laserPosition = true;
+        CONTEXT_AF.laser = document.querySelector("#laser");
+        CONTEXT_AF.laserPosition = true; //true: right, false: left
 
-        this.el.addEventListener('gripdown', this.swapLaser);
+        leftController.el.addEventListener('gripdown', this.leftLaser);
+        rightController.el.addEventListener('gripdown', this.rightLaser);
     },
-    swapLaser: function (evt) {
-        console.log(evt);
+    leftLaser: function (evt) {
+        if (laserPosition === true) {
+            CONTEXT_AF.laser.components.laser_controls.hand = "right";
+            laserPosition = false;
+        }
+    },
+
+    rightLaser: function (evt) {
+        if (laserPosition === false) {
+            CONTEXT_AF.laser.components.laser_controls.hand = "left";
+            laserPosition = true;
+        }
     }
 });
