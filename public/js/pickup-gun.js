bb7205c7b5743d3388d664790fd7c3051a1baf06
colinarelliott <colinarelliott@gmail.com>
2023-01-25 11:40:09 -0500

    added click the gun to pick it up

---
+++ public/js/pickup-gun.js
@@ -0,0 +1,23 @@
+//a script that allows you to pickup objects in the scene
+
+AFRAME.registerComponent('pickup-gun',{
+    schema : {
+    },
+
+    init: function () {
+        const CONTEXT_AF = this;
+        CONTEXT_AF.holdingGun = false;
+        CONTEXT_AF.gun = document.querySelector("#gun");
+        CONTEXT_AF.camera = document.querySelector("#camera");
+
+        CONTEXT_AF.el.addEventListener('click', function() {
+            //CONTEXT_AF.gun.flushToDom(); recommended online but not working...
+            var copy = CONTEXT_AF.gun.cloneNode();
+            CONTEXT_AF.camera.appendChild(copy);
+            CONTEXT_AF.gun.setAttribute("visible", false);
+            copy.setAttribute("position", "0.5 -0.25 -0.5");
+            copy.setAttribute("rotation", "0 110 0");
+        });
+        
+    }
+});
