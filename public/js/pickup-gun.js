3ed71693e7bd8d844249c68c7be136878bef693e
colinarelliott <colinarelliott@gmail.com>
2023-01-29 21:00:07 -0500

    made pickup gun work on HMD

--- public/js/pickup-gun.js
+++ public/js/pickup-gun.js
@@ -10,14 +10,22 @@ AFRAME.registerComponent('pickup-gun',{
         CONTEXT_AF.gun = document.querySelector("#gun");
         CONTEXT_AF.camera = document.querySelector("#camera");
         CONTEXT_AF.scene = document.querySelector("#scene");
+        CONTEXT_AF.rCon = document.querySelector("#otc-right");
 
         CONTEXT_AF.el.addEventListener('click', function() {
             //CONTEXT_AF.gun.flushToDom(); recommended online but not working...
             var copy = CONTEXT_AF.gun.cloneNode();
-            CONTEXT_AF.camera.appendChild(copy);
-            CONTEXT_AF.gun.setAttribute("visible", false);
-            copy.setAttribute("position", "0.5 -0.25 -0.5");
-            copy.setAttribute("rotation", "0 110 0");
+            if (navigator.userAgent.includes("Quest") === true) {
+                CONTEXT_AF.rCon.appendChild(copy);
+                CONTEXT_AF.gun.setAttribute("visible", false);
+                copy.setAttribute("position", "0 -0.1 0");
+                copy.setAttribute("rotation", "0 90 -45");
+            } else {
+                CONTEXT_AF.camera.appendChild(copy);
+                CONTEXT_AF.gun.setAttribute("visible", false);
+                copy.setAttribute("position", "0.5 -0.25 -0.5");
+                copy.setAttribute("rotation", "0 110 0");
+            }
             CONTEXT_AF.holdingGun = true;
             setTimeout(() => {  
                 CONTEXT_AF.scene.setAttribute("shoot-gun", "null");
