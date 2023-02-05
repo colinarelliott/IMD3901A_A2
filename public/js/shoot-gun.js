439fc37fe640726cc716569c46b802e2d7de5cfa
Colin Elliott <colinarelliott@gmail.com>
2023-02-05 12:35:01 -0500

    shooting target destroys it.

--- public/js/shoot-gun.js
+++ public/js/shoot-gun.js
@@ -7,9 +7,9 @@ AFRAME.registerComponent('shoot-gun',{
         CONTEXT_AF.gun = document.querySelector("#gun");
         CONTEXT_AF.camera = document.querySelector("#camera");
         CONTEXT_AF.pLaser = document.querySelector("#player-laser-wrapper");
+        CONTEXT_AF.target = document.querySelector("#target");
 
         CONTEXT_AF.el.addEventListener('click', function() {
-            console.log("bang!");
             CONTEXT_AF.gun.components.sound.playSound();
 
             //check to see if user is using Quest or not
@@ -17,14 +17,11 @@ AFRAME.registerComponent('shoot-gun',{
                 //code for quest collision detection check
             } else {
                 if (CONTEXT_AF.camera.components['collision-check'].data.isTarget === true) {
-                    console.log("hit target!");
-                    var target = document.querySelector("#" + CONTEXT_AF.camera.components['collision-check'].data.currentIntersected);
-                    target.setAttribute("visible", false);
-                    target.classList.remove("shootMe");
+                    console.log("hit " + CONTEXT_AF.target.id);
+                    CONTEXT_AF.target = document.querySelector("#" + CONTEXT_AF.camera.components['collision-check'].data.currentIntersected);
+                    CONTEXT_AF.target.remove();
                 }
-            }
-
-            
+            } 
         });
     }
 });
