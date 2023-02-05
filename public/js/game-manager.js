2fee1b2245d46876912a12643a7fcda38c7faed3
Colin Elliott <colinarelliott@gmail.com>
2023-02-05 14:19:24 -0500

    added scoreDisplay initialization on game start

--- public/js/game-manager.js
+++ public/js/game-manager.js
@@ -3,6 +3,7 @@
 AFRAME.registerComponent('game-manager' , { 
 
     schema : {
+        gameOn : {type: 'boolean', default: false},
         score : {type: 'number', default: 0},
     },
 
@@ -11,5 +12,21 @@ AFRAME.registerComponent('game-manager' , {
     },
 
     tick: function () {
+        const CONTEXT_AF = this;
+        if (CONTEXT_AF.gameOn === true) 
+        {
+            //update the score display
+            CONTEXT_AF.scoreDisplay.setAttribute("text", "value: score: " + CONTEXT_AF.data.score);
+
+            //check to see if there are any targets left
+            if (document.getElementsByClassName("shootMe").length === 0) {
+                //if there are no targets left, end the game
+                gameOn = false;
+                //display the end screen
+                //document.querySelector("#end-screen").setAttribute("visible", true);
+                //display the final score
+                //document.querySelector("#final-score").setAttribute("text", "value: " + CONTEXT_AF.data.score);
+            }
+        }
     }
 });
