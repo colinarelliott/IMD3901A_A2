c508d8f75ae264d4e4e4e428867aca4db93f2098
Colin Elliott <colinarelliott@gmail.com>
2023-02-05 12:41:07 -0500

    commented and completed shoot-gun.js

--- public/js/shoot-gun.js
+++ public/js/shoot-gun.js
@@ -1,14 +1,18 @@
+//this script handles shooting the gun and destroying targets based on collision-detection.js, WORKING!
+
 AFRAME.registerComponent('shoot-gun',{
     schema : {
     },
 
     init: function () {
         const CONTEXT_AF = this;
+        //get the gun, camera, player laser, and initial target
         CONTEXT_AF.gun = document.querySelector("#gun");
         CONTEXT_AF.camera = document.querySelector("#camera");
         CONTEXT_AF.pLaser = document.querySelector("#player-laser-wrapper");
         CONTEXT_AF.target = document.querySelector("#target");
 
+        //when the user clicks the trigger
         CONTEXT_AF.el.addEventListener('click', function() {
             CONTEXT_AF.gun.components.sound.playSound();
 
@@ -16,8 +20,9 @@ AFRAME.registerComponent('shoot-gun',{
             if (navigator.userAgent.includes("Quest") === true) {
                 //code for quest collision detection check
             } else {
+                //if the collided object is a target
                 if (CONTEXT_AF.camera.components['collision-check'].data.isTarget === true) {
-                    console.log("hit " + CONTEXT_AF.target.id);
+                    //remove the target
                     CONTEXT_AF.target = document.querySelector("#" + CONTEXT_AF.camera.components['collision-check'].data.currentIntersected);
                     CONTEXT_AF.target.remove();
                 }
