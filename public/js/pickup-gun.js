d5796ebb0af2fa3c22f16213e484c2ff998153f9
Colin Elliott <colinarelliott@gmail.com>
2023-02-05 11:31:42 -0500

    pickup-gun debugged and complete and commented

--- public/js/pickup-gun.js
+++ public/js/pickup-gun.js
@@ -1,4 +1,4 @@
-//a script that allows you to pickup objects in the scene
+//a script that allows you to pickup objects in the scene, WORKING!
 
 AFRAME.registerComponent('pickup-gun',{
     schema : {
@@ -13,23 +13,30 @@ AFRAME.registerComponent('pickup-gun',{
         CONTEXT_AF.rCon = document.querySelector("#otc-right");
 
         CONTEXT_AF.el.addEventListener('click', function() {
-            //CONTEXT_AF.gun.flushToDom(); recommended online but not working...
+            //make a copy of the gun
             var copy = CONTEXT_AF.gun.cloneNode();
+            //check to see if user is using Quest or not
             if (navigator.userAgent.includes("Quest") === true) {
+                //make the original gun invisible and add the copy to the right controller
                 CONTEXT_AF.rCon.appendChild(copy);
                 CONTEXT_AF.gun.setAttribute("visible", false);
+                CONTEXT_AF.gun = copy;
                 copy.setAttribute("position", "0 -0.1 0");
                 copy.setAttribute("rotation", "0 90 -45");
             } else {
+                //make the original gun invisible and add the copy to the camera
                 CONTEXT_AF.camera.appendChild(copy);
                 CONTEXT_AF.gun.setAttribute("visible", false);
+                CONTEXT_AF.gun = copy;
                 copy.setAttribute("position", "0.5 -0.25 -0.5");
                 copy.setAttribute("rotation", "0 110 0");
             }
             CONTEXT_AF.holdingGun = true;
+            //wait 500ms
             setTimeout(() => {  
+                //remove held gun's interactability, add shoot-gun component to scene
                 CONTEXT_AF.scene.setAttribute("shoot-gun", "null");
-                //CONTEXT_AF.gun.classList.remove(".interactable"); <- not working but need to remove gun's interactable attribute after picked up
+                CONTEXT_AF.gun.classList.remove("interactable"); 
             }, 500);
         });
         
